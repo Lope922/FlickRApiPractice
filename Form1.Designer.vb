cd5b118c95214365c3392b00b69cdfb9b1b3c65a
Lope <Lope922@gmail.com>
2015-04-11 01:48:37 -0500

    Messed around with stuff

---
+++ Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.getFlickrInfoBtn = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'getFlickrInfoBtn
+        '
+        Me.getFlickrInfoBtn.Location = New System.Drawing.Point(80, 37)
+        Me.getFlickrInfoBtn.Name = "getFlickrInfoBtn"
+        Me.getFlickrInfoBtn.Size = New System.Drawing.Size(115, 64)
+        Me.getFlickrInfoBtn.TabIndex = 0
+        Me.getFlickrInfoBtn.Text = "Get me some flickr Info "
+        Me.getFlickrInfoBtn.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(201, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(179, 159)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(412, 46)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(691, 341)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.getFlickrInfoBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents getFlickrInfoBtn As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Net
+Imports System.IO
+Imports FlickrNet
+Imports SimpleOAuth
+Imports System.Xml
+
+
+Public Class Form1
+    '' FLCIKR API KEY 4f60a04f101ef604ead9be84856d9519
+    '' FLICKR API SECRET 4e3fc31dff403d28
+
+
+
+    Dim apiKey As String = "4f60a04f101ef604ead9be84856d9519"
+
+    Dim apiSecret As String = "4e3fc31dff403d28"
+
+    Dim searchOptions As New PhotoSearchOptions
+
+    Dim searchCity As String = "Minneapolis"
+
+    Dim flickrThis As New Flickr(apiKey, apiSecret)
+
+    Function OAuthRequestToken() As String
+        Dim tempToken As String = ""
+        tempToken.GetType()
+        Return tempToken
+    End Function
+
+
+   
+    Public Function PhotosSearch(searchCity As String) As PhotoCollection
+
+
+
+    End Function
+
+
+    '   Calling this method will also clear AuthToken and set OAuthAccessToken and OAuthAccessTokenSecret. 
+    Public Function AuthOAuthGetAccessToken() As OAuthAccessToken
+     
+    End Function
+
+
+    Private Sub getFlickrInfoBtn_Click(sender As Object, e As EventArgs) Handles getFlickrInfoBtn.Click
+        Dim photoSearchOptions As New PhotoSearchOptions()
+        photoSearchOptions.Tags = "St. Louis"
+        photoSearchOptions.Page = 1
+        Dim cityPhotos As Photo = flickrThis.PhotosSearch(photoSearchOptions.Tags)
+        OAuthRequestToken(CType(apiKey))
+        MessageBox.Show("Here is the token" + OAuthRequestToken().ToString)
+        AuthOAuthGetAccessToken()
+        Try
+            If flickrThis.IsAuthenticated() = True Then
+                MessageBox.Show("Authentication is true")
+            Else
+                MessageBox.Show("Authentication is false")
+            End If
+
+            'flickrThis.TestEcho(apiKey)
+            ' flickrThis.AuthOAuthCheckToken()
+            flickrThis.PhotosSearch(photoSearchOptions)
+        Catch authenticationProblem As AuthenticationRequiredException
+            MessageBox.Show(authenticationProblem.Message)
+        End Try
+
+
+
+    End Sub
+End Class
