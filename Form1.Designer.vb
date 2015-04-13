b1a0403f2a894ae5f2c177017519e698971b2d05
Lope <Lope922@gmail.com>
2015-04-13 07:21:06 -0500

    added 2 more search results to practice Xpath navigation and added values to hand the xpaths. See bookmarked tab or parallel web requests. Also build request token still. to send initial request with city parameters

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,10 +25,14 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.getFlickrInfoBtn = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.stateComboBox = New System.Windows.Forms.ComboBox()
+        Me.txtBoxCityName = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'getFlickrInfoBtn
@@ -42,24 +46,12 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(256, 80)
+        Me.PictureBox1.Location = New System.Drawing.Point(289, 21)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(179, 159)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Right
-        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
-        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(618, 70)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
-        Me.DataGridView1.TabIndex = 2
         '
         'ImageList1
         '
@@ -67,27 +59,69 @@ Partial Class Form1
         Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
         Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
         '
+        'stateComboBox
+        '
+        Me.stateComboBox.FormattingEnabled = True
+        Me.stateComboBox.Items.AddRange(New Object() {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"})
+        Me.stateComboBox.Location = New System.Drawing.Point(80, 159)
+        Me.stateComboBox.Name = "stateComboBox"
+        Me.stateComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.stateComboBox.TabIndex = 2
+        '
+        'txtBoxCityName
+        '
+        Me.txtBoxCityName.Location = New System.Drawing.Point(80, 121)
+        Me.txtBoxCityName.Name = "txtBoxCityName"
+        Me.txtBoxCityName.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxCityName.TabIndex = 3
+        Me.txtBoxCityName.Text = "Enter City Name"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(486, 21)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(179, 159)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(686, 21)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(179, 159)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.ClientSize = New System.Drawing.Size(897, 389)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.txtBoxCityName)
+        Me.Controls.Add(Me.stateComboBox)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.getFlickrInfoBtn)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents getFlickrInfoBtn As System.Windows.Forms.Button
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
     Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents stateComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents txtBoxCityName As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -5,49 +5,49 @@ Imports SimpleOAuth
 Imports System.Xml
 Imports System.Xml.XPath
 
+'' still need to add a form1 event loader that handles authentication of temp APIKey. 
+'' also need to setup background workers and handle errors
 
 
 Public Class Form1
     '' FLCIKR API KEY 4f60a04f101ef604ead9be84856d9519
     '' FLICKR API SECRET 4e3fc31dff403d28
 
-   
-    'Dim tokenStep1 As String = ""
+    Public Function city() As String
+        Dim cityFromTextBox As String
+        cityFromTextBox = txtBoxCityName.Text
 
-    'Dim webRequestToken As WebRequest = WebRequest.Create(tokenStep1)
+        If cityFromTextBox.Contains(" ") Then
+            cityFromTextBox.Replace(" ", "+")
+            Return cityFromTextBox
+        End If
+    End Function
 
-    'Dim tokenResponseStream As Stream = webRequestToken.GetResponse.GetResponseStream()
+    Public Function state() As String
+        Dim stateFromComboBox As String
+        stateFromComboBox = CStr(stateComboBox.SelectedItem())
+        Return stateFromComboBox
+    End Function
 
-    'Dim httpDocument As HtmlDocument
-
-
-
-    'Private Sub WriteNewDocument(httpDocument As HtmlDocument)
-    '    If (WebBrowser1.Document IsNot Nothing) Then
-    '        Dim doc As HtmlDocument = tokenResponseStream.Write.Document.OpenNew(True)
-    '        doc.Write("<HTML><BODY>This is a new HTML document.</BODY></HTML>")
-    '    End If
-    'End Sub
-
-    'Dim requestedTest As String = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=df52b9fa8e1632a530e801c286f20f7c&tags=St.+Louis%2C+MO&privacy_filter=1&safe_search=1&content_type=1&per_page=1&page=1&format=rest&auth_token=72157651508263228-ecffc79596f9a26f&api_sig=a8ace12d9ba340200b4567676e2664b7"
-
-    'Dim flickrRequestPhoto As WebRequest = HttpWebRequest.Create(requestedTest)
-
-    'Dim testResponseStream As Stream = flickrRequestPhoto.GetResponse.GetResponseStream()
-
-    'Dim xmlReader As New XmlDocument
-    '    xmlReader.Load(testResponseStream)
-
-
-
+    Public Function callCityAndState() As String
+        city()
+        state()
+        MessageBox.Show("city and state " + city() + state())
+    End Function
 
 
     Private Sub getFlickrInfoBtn_Click(sender As Object, e As EventArgs) Handles getFlickrInfoBtn.Click
+        '' call city and state fuctions 
+        callCityAndState()
+        Dim 
+
 
         Try
+
             '' still need to rebuild using the flickr.photo.search method with custom search options similar to ones specified here. 
             '' this search request is for St. Louis, MO. this particular method does not need to be authorized. and most parameters are optional. 
-            Dim requestedTest As String = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=df52b9fa8e1632a530e801c286f20f7c&tags=St.+Louis%2C+MO&privacy_filter=1&safe_search=1&content_type=1&per_page=1&page=1&format=rest&auth_token=72157651508263228-ecffc79596f9a26f&api_sig=a8ace12d9ba340200b4567676e2664b7"
+            Dim requestedTest As String = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=32036da6f917a5a5bf879ce5ba1b6863&tags=St.+Paul%2C+MN&safe_search=1&content_type=1&media=photos&has_geo=-93.08209991&lat=44.97010040&radius_units=mi&per_page=3&page=1&format=rest"
+
 
             '' create the web request using the above url 
             Dim flickrRequestPhoto As WebRequest = WebRequest.Create(requestedTest)
@@ -72,28 +72,34 @@ Public Class Form1
             '' need to obtain xml-rpc siblings at the following values 
             ''secret path single node request = //photo/@secret this returns the location. Need to access inner text/xml by calling the innner text method 
 
-            Dim url_builder_for_photo_secret_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@secret")
-
-            ''Still need to create 
-
+            Dim url_builder_for_photo_secret1_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@secret")
+            Dim url_builder_for_photo_secret2_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@secret")
+            Dim url_builder_for_photo_secret3_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@secret")
 
             'server id' 
-            Dim url_builder_for_photo_serverID_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@server")
-
+            Dim url_builder_for_photo_serverID1_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@server")
+            Dim url_builder_for_photo_serverID2_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@server")
+            Dim url_builder_for_photo_serverID3_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@server")
             'id'
-            Dim url_builder_for_photo_ID_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@id")
+            Dim url_builder_for_photo_ID1_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@id")
+            Dim url_builder_for_photo_ID2_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@id")
+            Dim url_builder_for_photo_ID3_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@id")
 
             ' Dim xmlRpcElemet As XmlNodeList = xmlReader.SelectNodes("//photos/@farm")
+
             '' use this to request farm value 
-            Dim url_builder_for_farm_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@farm")
+            Dim url_builder_for_farm1_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@farm")
+            Dim url_builder_for_farm2_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@farm")
+            Dim url_builder_for_farm3_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@farm")
+
 
             '' had alot of trouble trying to figure out how to set this up to read each element sibling. Had to use innerxml to extract sibling information on the specified path. 
             '' see example of return data at top of txtFile1
 
-            MessageBox.Show("Here is Farm: " + url_builder_for_farm_valueXML.InnerXml + vbCrLf +
-                            "Here is secret:" + url_builder_for_photo_secret_value_from_XML.InnerXml + vbCrLf _
-                            + "Server ID: " + url_builder_for_photo_serverID_value_from_XML.InnerXml + vbCrLf +
-                            "ID :" + url_builder_for_photo_ID_valueXML.InnerXml)
+            'MessageBox.Show("Here is Farm: " + url_builder_for_farm1_valueXML.InnerXml + vbCrLf +
+            '                "Here is secret:" + url_builder_for_photo_secret1_value_from_XML.InnerXml + vbCrLf _
+            '                + "Server ID: " + url_builder_for_photo_serverID1_value_from_XML.InnerXml + vbCrLf +
+            '                "ID :" + url_builder_for_photo_ID1_valueXML.InnerXml)
 
             '' confirmation that i obtained the desired values needed to request a photo. 
 
@@ -101,15 +107,23 @@ Public Class Form1
 
             ' https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 
-            Dim url_PhotoRequest As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm_valueXML.InnerXml,
-                                                           url_builder_for_photo_serverID_value_from_XML.InnerXml, url_builder_for_photo_ID_valueXML.InnerXml,
-                                                           url_builder_for_photo_secret_value_from_XML.InnerXml)
+            'For Each number As Integer In 1 to 3
 
+            Dim url_PhotoRequest As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm1_valueXML.InnerXml,
+                                                               url_builder_for_photo_serverID1_value_from_XML.InnerXml, url_builder_for_photo_ID1_valueXML.InnerXml,
+                                                               url_builder_for_photo_secret1_value_from_XML.InnerXml)
             ''render this image to an image box to be displayed on a bing maps api. Or look into using Flickrs maps 
 
-            '"Copy and past this link into a browser and see what photo is returned " + url_PhotoRequest)
+            '"Copy and paste this link into a browser and see what photo is returned " + url_PhotoRequest)
             '' tried to display to messagebox , but could not click on its contents. 
 
+            Dim PhotoRequest As WebRequest = WebRequest.Create(url_PhotoRequest)
+
+            Dim photoResponseStream As Stream = PhotoRequest.GetResponse.GetResponseStream()
+
+            Dim flickrPhotoFromStream As Image = Image.FromStream(photoResponseStream)
+
+            PictureBox1.Image = flickrPhotoFromStream
 
             'error 404 page not found 
             '' need a try catch block when i reach this point. 
