3f6f18b69083b0e3b375ece315f7052acc44469f
Lope <Lope922@gmail.com>
2015-04-18 17:00:23 -0500

    got to a breakpoint. need to further work on loop to iterate over the node elements and retrieve information.  got to a point where i am stuck trying to figure out how to request each photo.

--- Form1.vb
+++ Form1.vb
@@ -3,7 +3,7 @@ Imports System.IO
 Imports System.Xml
 Imports System.Xml.XPath
 
-'' still need to add a form1 event loader that handles authentication of temp APIKey. 
+
 '' also need to setup background workers and handle errors
 
 
@@ -112,152 +112,128 @@ Public Class Form1
 
 
 
-        Try
-            '' LETS TRY AND IMPORT MY CLASS AND USE IT.... 
+        '  Try @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        '' LETS TRY AND IMPORT MY CLASS AND USE IT.... 
 
+        '' create a new instance of the flickrclass to call its functions 
+        Dim testClass As New flickRportion
 
+        '' create a location to save the response  using the predefined search settings
+        Dim response = testClass.searchSettings()
 
-            '' still need to rebuild using the flickr.photo.search method with custom search options similar to ones specified here. 
-            '' this search request is for St. Louis, MO. this particular method does not need to be authorized. and most parameters are optional. 
-            Dim requestedTest As String = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c4471b9774deb717aa6c61f6b88e259a&tags=St.Paul&per_page=3&page=1&format=rest"
+        '' send the request for the city search 
+        testClass.send_request_for_photos(response)
 
+        '' now try and read from the response using the extractInfoFromStream function 
+        Dim extractedinfo = response
 
-
-            '' create the web request using the above url 
-            Dim flickrRequestPhoto As WebRequest = WebRequest.Create(requestedTest)
-
-            '' store the response stream in the created variable
-            Dim testResponseStream As Stream = flickrRequestPhoto.GetResponse.GetResponseStream()
-
-            '' create a xmldocument to format the response
-            Dim xmlReader As New XmlDocument
-
-            '' select the single xml-rpc response node
-            ' Dim xmlRPCNode As XmlNode
-
-            xmlReader.Load(testResponseStream)
-
-
-            ' Save the document to a file. White space is 
-            ' preserved (no white space). = true 
-            xmlReader.PreserveWhitespace = False '' set to false to preserve the same format as formatted response 
-            xmlReader.Save("data.xml") '' save the document in the debugger and use response to practice extracting info. 
-
-            '' need to obtain xml-rpc siblings at the following values 
-            ''secret path single node request = //photo/@secret this returns the location. Need to access inner text/xml by calling the innner text method 
+        '' need to rename because it isn't requesting photos yet. That will need to be another fucntion/method. 
+        '  testClass.send_request_for_photos(extractedinfo)
 
 
 
 
-            Dim url_builder_for_photo_secret1_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@secret")
-            Dim url_builder_for_photo_secret2_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@secret")
-            Dim url_builder_for_photo_secret3_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@secret")
+        testClass.extractInfoFromStream()
 
-            'server id' 
-            Dim url_builder_for_photo_serverID1_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@server")
-            Dim url_builder_for_photo_serverID2_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@server")
-            Dim url_builder_for_photo_serverID3_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@server")
-            'id'
-            Dim url_builder_for_photo_ID1_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@id")
-            Dim url_builder_for_photo_ID2_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@id")
-            Dim url_builder_for_photo_ID3_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@id")
+        '' next try reading from stream to picture boxes. 
 
-            ' Dim xmlRpcElemet As XmlNodeList = xmlReader.SelectNodes("//photos/@farm")
+        MessageBox.Show("work done")
 
-            '' use this to request farm value 
-            Dim url_builder_for_farm1_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@farm")
-            Dim url_builder_for_farm2_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@farm")
-            Dim url_builder_for_farm3_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@farm")
+        '' THIS IS THE NEXT METHOD TO WRITE... SEE PREVIOUS NOTES TO WRITE TO A HASHTABLE OR HASHMAPE TO GET EACH RESPONSE FORMAT. 
 
 
-            '' had alot of trouble trying to figure out how to set this up to read each element sibling. Had to use innerxml to extract sibling information on the specified path. 
-            '' see example of return data at top of txtFile1
-
-            'MessageBox.Show("Here is Farm: " + url_builder_for_farm1_valueXML.InnerXml + vbCrLf +
-            '                "Here is secret:" + url_builder_for_photo_secret1_value_from_XML.InnerXml + vbCrLf _
-            '                + "Server ID: " + url_builder_for_photo_serverID1_value_from_XML.InnerXml + vbCrLf +
-            '                "ID :" + url_builder_for_photo_ID1_valueXML.InnerXml)
-
-            '' confirmation that i obtained the desired values needed to request a photo. 
-
-            '' now build a url request in this format. 
-
-            ' https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-
-            'For Each number As Integer In 1 to 3
 
 
-            '' TODO : FOR EACH URL PHOTO READ AS A STREAM AND PROCESS EACH PHOTO. 
+        '        Dim url_builder_for_photo_secret1_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@secret")
+        '        Dim url_builder_for_photo_secret2_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@secret")
+        '        Dim url_builder_for_photo_secret3_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@secret")
 
-            Dim url_PhotoRequest1 As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm1_valueXML.InnerXml,
-                                                               url_builder_for_photo_serverID1_value_from_XML.InnerXml, url_builder_for_photo_ID1_valueXML.InnerXml,
-                                                               url_builder_for_photo_secret1_value_from_XML.InnerXml)
+        '        'server id' 
+        '        Dim url_builder_for_photo_serverID1_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo/@server")
+        '        Dim url_builder_for_photo_serverID2_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@server")
+        '        Dim url_builder_for_photo_serverID3_value_from_XML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@server")
+        '        'id'
+        '        Dim url_builder_for_photo_ID1_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@id")
+        '        Dim url_builder_for_photo_ID2_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@id")
+        '        Dim url_builder_for_photo_ID3_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@id")
 
-            Dim url_PhotoRequest2 As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm2_valueXML.InnerXml,
-                                                             url_builder_for_photo_serverID2_value_from_XML.InnerXml, url_builder_for_photo_ID2_valueXML.InnerXml,
-                                                             url_builder_for_photo_secret2_value_from_XML.InnerXml)
-            Dim url_PhotoRequest3 As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm3_valueXML.InnerXml,
-                                                             url_builder_for_photo_serverID3_value_from_XML.InnerXml, url_builder_for_photo_ID3_valueXML.InnerXml,
-                                                             url_builder_for_photo_secret3_value_from_XML.InnerXml)
+        '        ' Dim xmlRpcElemet As XmlNodeList = xmlReader.SelectNodes("//photos/@farm")
 
-            ''render this image to an image box to be displayed on a bing maps api. Or look into using Flickrs maps 
-
-            '"Copy and paste this link into a browser and see what photo is returned " + url_PhotoRequest)
-            '' tried to display to messagebox , but could not click on its contents. 
-
-            Dim PhotoRequest1 As WebRequest = WebRequest.Create(url_PhotoRequest1)
-            Dim photoResponseStream As Stream = PhotoRequest1.GetResponse.GetResponseStream()
-            Dim flickrPhotoFromStream As Image = Image.FromStream(photoResponseStream)
-            PictureBox1.Image = flickrPhotoFromStream
-
-            Dim PhotoRequest2 As WebRequest = WebRequest.Create(url_PhotoRequest2)
-            Dim photoResponseStream2 As Stream = PhotoRequest2.GetResponse.GetResponseStream()
-            Dim flickrPhotoFromStream2 As Image = Image.FromStream(photoResponseStream2)
-            PictureBox2.Image = flickrPhotoFromStream2
-
-            Dim PhotoRequest3 As WebRequest = WebRequest.Create(url_PhotoRequest3)
-            Dim photoResponseStream3 As Stream = PhotoRequest3.GetResponse.GetResponseStream()
-            Dim flickrPhotoFromStream3 As Image = Image.FromStream(photoResponseStream3)
-            PictureBox3.Image = flickrPhotoFromStream3
+        '        '' use this to request farm value 
+        '        Dim url_builder_for_farm1_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo/@farm")
+        '        Dim url_builder_for_farm2_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[2]/@farm")
+        '        Dim url_builder_for_farm3_valueXML As XmlNode = xmlReader.SelectSingleNode("//photo[3]/@farm")
 
 
-            'error 404 page not found 
-            '' need a try catch block when i reach this point. 
-            ''todo get the rest of the elements from the initial response. 
+        '        '' had alot of trouble trying to figure out how to set this up to read each element sibling. Had to use innerxml to extract sibling information on the specified path. 
+        '        '' see example of return data at top of txtFile1
+
+        '        'MessageBox.Show("Here is Farm: " + url_builder_for_farm1_valueXML.InnerXml + vbCrLf +
+        '        '                "Here is secret:" + url_builder_for_photo_secret1_value_from_XML.InnerXml + vbCrLf _
+        '        '                + "Server ID: " + url_builder_for_photo_serverID1_value_from_XML.InnerXml + vbCrLf +
+        '        '                "ID :" + url_builder_for_photo_ID1_valueXML.InnerXml)
+
+        '        '' confirmation that i obtained the desired values needed to request a photo. 
+
+        '        '' now build a url request in this format. 
+
+        '        ' https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+        '        'For Each number As Integer In 1 to 3
 
 
-            ''NOTE: each web request sent generates a different response of results. 
+        '        '' TODO : FOR EACH URL PHOTO READ AS A STREAM AND PROCESS EACH PHOTO. 
+
+        '        Dim url_PhotoRequest1 As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm1_valueXML.InnerXml,
+        '                                                           url_builder_for_photo_serverID1_value_from_XML.InnerXml, url_builder_for_photo_ID1_valueXML.InnerXml,
+        '                                                           url_builder_for_photo_secret1_value_from_XML.InnerXml)
+
+        '        Dim url_PhotoRequest2 As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm2_valueXML.InnerXml,
+        '                                                         url_builder_for_photo_serverID2_value_from_XML.InnerXml, url_builder_for_photo_ID2_valueXML.InnerXml,
+        '                                                         url_builder_for_photo_secret2_value_from_XML.InnerXml)
+        '        Dim url_PhotoRequest3 As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", url_builder_for_farm3_valueXML.InnerXml,
+        '                                                         url_builder_for_photo_serverID3_value_from_XML.InnerXml, url_builder_for_photo_ID3_valueXML.InnerXml,
+        '                                                         url_builder_for_photo_secret3_value_from_XML.InnerXml)
+
+        '        ''render this image to an image box to be displayed on a bing maps api. Or look into using Flickrs maps 
+
+        '        '"Copy and paste this link into a browser and see what photo is returned " + url_PhotoRequest)
+        '        '' tried to display to messagebox , but could not click on its contents. 
+
+        '        Dim PhotoRequest1 As WebRequest = WebRequest.Create(url_PhotoRequest1)
+        '        Dim photoResponseStream As Stream = PhotoRequest1.GetResponse.GetResponseStream()
+        '        Dim flickrPhotoFromStream As Image = Image.FromStream(photoResponseStream)
+        '        PictureBox1.Image = flickrPhotoFromStream
+
+        '        Dim PhotoRequest2 As WebRequest = WebRequest.Create(url_PhotoRequest2)
+        '        Dim photoResponseStream2 As Stream = PhotoRequest2.GetResponse.GetResponseStream()
+        '        Dim flickrPhotoFromStream2 As Image = Image.FromStream(photoResponseStream2)
+        '        PictureBox2.Image = flickrPhotoFromStream2
+
+        '        Dim PhotoRequest3 As WebRequest = WebRequest.Create(url_PhotoRequest3)
+        '        Dim photoResponseStream3 As Stream = PhotoRequest3.GetResponse.GetResponseStream()
+        '        Dim flickrPhotoFromStream3 As Image = Image.FromStream(photoResponseStream3)
+        '        PictureBox3.Image = flickrPhotoFromStream3
 
 
-            'catch any exceptions and let me know what is going wrong. 
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
+        '        'error 404 page not found 
+        '        '' need a try catch block when i reach this point. 
+        '        ''todo get the rest of the elements from the initial response. 
 
+
+        '        ''NOTE: each web request sent generates a different response of results. 
+
+
+        '        'catch any exceptions and let me know what is going wrong. 
+        '    Catch ex As Exception
+        '        MessageBox.Show(ex.Message)
+        '    End Try
+
+
+        ''##############################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#######################################@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-        '' create a web request for the authentication and get an access token to create an initial request 
-        'OAuth.OAuthBase.SignatureTypes.HMACSHA1.ToString()
 
-        'Try
-
-        '    Dim tokenRequest As WebRequest = WebRequest.Create("https://api.flickr.com/services/rest/?method=flickr.test.echo&api_key=4f60a04f101ef604ead9be84856d9519&format=rest&auth_token=72157651870085376-429a63a14d709dcb&api_sig=7034a717fe5c4d991bdb4c8a641ba986")
-        '  searchSettings()
-
-        '    Dim f As Flickr = New Flickr("4f60a04f101ef604ead9be84856d9519")
-
-        '    MessageBox.Show("Authentication successful")
-
-        '        Catch authenticationProblem As Exception
-        'MessageBox.Show(authenticationProblem.Message)
-
-
-
-        'End Try
-    End Sub
-
-   
 
 
 
@@ -270,4 +246,7 @@ Public Class Form1
 
         MessageBox.Show("This testing hasn't been initialized ", "Hey man!", MessageBoxButtons.OK, MessageBoxIcon.Hand)
     End Sub
+
+   
+
 End Class
--- flickRportion.vb
+++ flickRportion.vb
@@ -8,7 +8,7 @@ Public Class flickRportion
     '' for flikr i will need a web request for the city and state search built on these qualities. 
 
     '' this returns the string built ready to be sent. NO ERROR CHECKING PRIOR. 
-    Private Function searchSettings() As String
+    Public Function searchSettings() As String
         ' use this base url to generate a request. Replace api_ key with my api key 
         '       URL: https://api.flickr.com/services/rest/?method=flickr.photos.search
         '&api_key=2c33c4c48c9fa2f189c0c08e45be9322
@@ -36,8 +36,11 @@ Public Class flickRportion
     '' this does not return an exception on all paths. WHY?
 
 
+
     '' this function needs access to the search settings because that is what they need to be sent as. May want to consider renaming. 
-    Private Function send_request_for_photos(searchsettings As String) As Stream
+    'ACCEPTS: SEARCHSETTINGS == STRING URLBUILT WITH SEARCH SETTINGS DEFINED WITHIN THE URL 
+    'RETURNS: AN XMLDOCUMENT WITH A RESPONSE FROM THE WEBSITE. 
+    Public Function send_request_for_photos(searchsettings As String) As XmlDocument
         '' this function will return the photos as a stream because that is how i want to read them in order to be able to print to each photo box 
 
         ' create the web request first 
@@ -50,24 +53,88 @@ Public Class flickRportion
         '' step 1 create the stream from photos 
         Dim xmlResponse As Stream = flickrInitialrequest.GetResponse.GetResponseStream()
 
-        Dim xmlweatherReader As New XmlDocument
-        xmlweatherReader.Load(xmlResponse)
+        Dim xmlflickrReader As New XmlDocument
+        xmlflickrReader.Load(xmlResponse)
 
 
-
+        ' Save the document to a file. White space is 
+        '   preserved (no white space). = true 
+        xmlflickrReader.PreserveWhitespace = False '' set to false to preserve the same format as formatted response 
+        xmlflickrReader.Save("flickrinitialRequest.txt") '' save the document in the debugger and use response to practice extracting
 
         '' save the response to a document called.. and read it 
         '' get the filewriter
-        Dim file As System.IO.StreamWriter
-        '' write the file to a file with the file name 
-        file = My.Computer.FileSystem.OpenTextFileWriter("C:\Desktop\FirstBreakPoint.txt", True)
-        file.WriteLine(xmlResponse)
-        file.Close()
+        'Dim file As System.IO.StreamWriter
+        ' '' write the file to a file with the file name 
+        'file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\Lope\Desktop\FirstBreakPoint.txt", True)
+        'file.WriteLine(xmlResponse)
+        'file.Close()
         '' now save to a file to see what is being written and also confirm. then search through and sort the info needed with the next loop to be created
 
-
+        '' don't forget to close the connection 
+        xmlResponse.Close()
 
         '' todo create the loop that loops through the data and saves/ maps them. Think about using a hashtable or hashmap. It could be useful here plus i need the practice.
+
+        Return xmlflickrReader
+        '' this is where i am stuck and may need to refer to a hashmap 
+    End Function
+
+    '' get the info and return it either as a node list. or a stream to read and write to the photo boxes 
+    Public Function extractInfoFromStream() As XmlNodeList
+        Try
+            '' read from the stream 
+            Dim xmlResponseDocument As New XmlDocument
+            xmlResponseDocument.Load("flickrinitialRequest.txt")
+
+
+            'doc.Load("C:\Users\Lope\Desktop\flickrinitialRequest.xml")
+
+            '' so for each element for a photo request create a request. 
+
+            '' declare the location of each node that i accessed to request a photo.
+
+            For node_in_nodeLIst As Integer = 1 To 6 Step 1
+
+
+                '' lets try an enhanced for loop 
+
+                Dim secret As XmlNodeList = xmlResponseDocument.SelectNodes("//photo/@secret")
+
+                '' now that the innnertext from that node and save it to request a photo. 
+                Dim farm As XmlNodeList = xmlResponseDocument.SelectNodes("//photo/@farm")
+
+                Dim server As XmlNodeList = xmlResponseDocument.SelectNodes("//photo/@sever")
+
+                Dim photoid As XmlNodeList = xmlResponseDocument.SelectNodes("//photo/@id")
+
+            Next
+            Dim photo_url_request As String = String.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg", farm, server, photoid, secret)
+
+            '' now for each node in nodelist create web reqeust using the formate above and 
+
+            MessageBox.Show("Here is the secret test" + secret.ToString)
+
+            Return secret
+            MessageBox.Show("Here:" + secret.ToString)
+
+        Catch xmlpath As XmlException
+            MessageBox.Show("problem requesting nodes. Contact developer ")
+        End Try
+        '  Dim nodeList As XmlNodeList
+
+        'Dim root As XmlNode = doc.DocumentElement
+
+        For Number As Integer = 1 To 6 Step 1
+            ' load the xmlResponse and reach eash number from the stream. 
+            '' select single node number 
+            '' unless number equals 1 then return that single node 
+
+            '' else format each request per number{#} format. 
+        Next
+
+
+
 
 
 
